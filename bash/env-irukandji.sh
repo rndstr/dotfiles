d0e883b5dbf1d0550237c94876a13292d6f05c62
Roland Schilter <mail@rolandschilter.ch>
2010-09-23 10:44:01 +0200

    added bash env for irukandji

---
+++ bash/env-irukandji.sh
@@ -0,0 +1,6 @@
+#export PROMPT_HOSTNAME_COLOR='1;37m'
+#export PROMPT_SUFFIX_COUNT=1
+export DOTFILES_ENV_DIST='archlinux'
+export DOTFILES_ENV_DOMAIN='liip'
+export DOTFILES_ENV_TYPE='desktop'
+source ~/.profile
