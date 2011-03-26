5064d6e7a60a4b54009d546ddf1b0ee44b34ac2f
Roland Schilter <r@rndstr.org>
2011-03-26 20:35:52 +0100

    added lamprey bash env

---
+++ bash/env-lamprey.sh
@@ -0,0 +1,6 @@
+#export PROMPT_HOSTNAME_COLOR='1;37m'
+export PROMPT_SUFFIX_COUNT=3
+export DOTFILES_ENV_DIST='archlinux'
+export DOTFILES_ENV_DOMAIN='tank'
+export DOTFILES_ENV_TYPE='server'
+[ -f ~/.profile ] && source ~/.profile
