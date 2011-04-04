e4b40799b3dc69f4f26b16ebd80ef49d430cd2e0
Roland Schilter <r@rndstr.org>
2011-04-04 22:11:03 +0200

    k

--- bash/env.d/dist/archlinux/aliases.sh
+++ bash/env.d/dist/archlinux/aliases.sh
@@ -1,5 +1,5 @@
 alias pm="sudo pacman"
-alias aurget="sudo --asroot"
+alias aurget="sudo aurget --asroot"
 function istart { sudo /etc/rc.d/$* start; }
 function irestart { sudo /etc/rc.d/$* restart; }
 function istop { sudo /etc/rc.d/$* stop; }
