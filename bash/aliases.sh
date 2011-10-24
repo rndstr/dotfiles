84225a132c8cd1e765e4bb24a843cc0589f691ae
Roland Schilter <roland.schilter@liip.ch>
2011-10-24 13:13:48 +0200

    `f' for find alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -74,6 +74,8 @@ function fbup { while [ $# -gt 0 ]; do cp -i "$1" "$1-"; shift; done; }
 
 function TODO() { echo $* >> $HOME/.TODO; }
 
+function f() { find -iname "*$**"; }
+
 # open all matching files with editor
 function grepvi() { $EDITOR $(grep $* * | cut -d : -f 1 | uniq); }
 
