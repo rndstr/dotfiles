61d00bd0c83d77c6ed3f1844c77ca3a394921e9a
Roland Schilter <roli@schilter.me>
2013-04-05 10:38:10 -0700

    tunnel

--- zsh/functions/vpn.sh
+++ zsh/functions/vpn.sh
@@ -1,8 +1,9 @@
 function thromium {
-    export SOCKS_SERVER=lamprey:1080
+    host=rndstr
+    export SOCKS_SERVER=$HOST:1080
     export SOCKS_VERSION=5
     echo ">>> starting tunnel in background"
-    ssh -D 1080 -f -C -q -N lamprey &
+    ssh -D 1080 -f -C -q -N $HOST &
     PID=$!
     chromium
     echo -n ">>> killing tunnel"
