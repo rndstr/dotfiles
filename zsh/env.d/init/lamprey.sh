2b8006bb8198e7779189da7066a0f6d0fdddc2ec
Roland Schilter <roli@schilter.me>
2013-08-17 11:23:31 +0200

    [zsh] lamprey now using zsh

---
+++ zsh/env.d/init/lamprey.sh
@@ -0,0 +1,11 @@
+#export PROMPT_HOSTNAME_COLOR='1;37m'
+#export PROMPT_SUFFIX_COUNT=1
+export DOTFILES_ENV_DIST='arch'
+export DOTFILES_ENV_DOMAIN='tank'
+export DOTFILES_ENV_TYPE='server'
+
+# rvm
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+[[ -r "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
+PS1="\$($HOME/.rvm/bin/rvm-prompt) $PS1"
