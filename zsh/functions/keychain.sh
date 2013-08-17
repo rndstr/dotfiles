51dcc679e8c869a0ec310a9d1c7ef4913e244f52
Roland Schilter <roli@schilter.me>
2013-08-17 11:19:18 +0200

    [zsh] replace `which` with `command` for compatibility

--- zsh/functions/keychain.sh
+++ zsh/functions/keychain.sh
@@ -3,7 +3,7 @@
 #[ $IS_LOGIN_SHELL = 'yes' ] || return 0
 
 # because this script runs as part of core, avoid writing to stdout
-if which keychain 1>/dev/null 2>&1; then
+if command -v keychain 1>/dev/null 2>&1; then
   if [ -f ~/.ssh/autoload ]; then
     keychain -Q --nogui -q $(<~/.ssh/autoload) 1>&2
     source ~/.keychain/$(hostname)-sh
