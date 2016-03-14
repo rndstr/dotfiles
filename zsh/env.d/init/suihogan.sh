cdaba80df2d0d17ee744a010e3273e63583e7a68
Roland Schilter <roli@schilter.me>
2016-03-14 18:12:04 +0100

    new device suihogan

---
+++ zsh/env.d/init/suihogan.sh
@@ -0,0 +1,9 @@
+#export PROMPT_HOSTNAME_COLOR='1;37m'
+#export PROMPT_SUFFIX_COUNT=1
+export DOTFILES_ENV_DIST='arch'
+export DOTFILES_ENV_DOMAIN='tank'
+export DOTFILES_ENV_TYPE='laptop'
+
+export GOPATH=/home/rndstr/dev/golang
+export ANDROID_HOME=$HOME/apps/android-sdk
+PATH=$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$HOME/apps/android-studio/bin
