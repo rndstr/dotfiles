666a29eeb6abf84ef65fcdfdbc3cabf7a03632a1
Roland Schilter <roli@schilter.me>
2016-04-04 09:48:06 +0200

    [zsh] rvm is super slow :(

--- zsh/init.d/rvm.sh
+++ zsh/init.d/rvm.sh
@@ -1,4 +1,4 @@
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-[[ -r "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
-PS1="\$($HOME/.rvm/bin/rvm-prompt) $PS1"
+#[[ -r "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
+#PS1="\$($HOME/.rvm/bin/rvm-prompt) $PS1"
