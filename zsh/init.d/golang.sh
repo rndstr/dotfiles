adf26c2392a86650158f6d97c2021f4a2364ff3c
Roland Schilter <roli@schilter.me>
2017-06-02 00:02:59 +0200

    [go] default GOPATH

--- zsh/init.d/golang.sh
+++ zsh/init.d/golang.sh
@@ -1,2 +1,8 @@
-export GOPATH=/home/rndstr/dev/golang
+export GOPATH=/home/rndstr/weave/golang
 PATH=$PATH:$GOPATH/bin
+
+l.go() {
+    export GOPATH=~/dev/golang
+    export PATH=$PATH:~/dev/golang/bin
+}
+
