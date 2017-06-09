7456b37d5e7b6e2eb892e683a9c067567478382d
Roland Schilter <roli@schilter.me>
2017-06-09 12:59:03 +0100

    [zsh] minikub functions

--- zsh/init.d/k8s.sh
+++ zsh/init.d/k8s.sh
@@ -1,1 +1,19 @@
 source <(kubectl completion zsh)
+
+function mk.start() {
+  minikube start --vm-driver=virtualbox --memory=4096
+  minikube ssh -- sudo ln -s /hosthome/$USER /home/$USER
+}
+
+function mk.env() {
+  eval $(minikube docker-env)
+}
+
+function mk.load() {
+  docker images |
+  awk '$1 ~ "^quay.io/weaveworks" && $2 == "latest" {print $1 ":" $2}' |
+  while read img; do
+    docker save $img |
+    (eval $(minikube docker-env) && docker load)
+  done
+}
