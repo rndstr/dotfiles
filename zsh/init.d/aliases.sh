1a161e866309f794eb7e4d957ec3c841c35044be
Roland Schilter <roli@schilter.me>
2017-06-22 10:39:12 +0200

    [zsh/k8s] more aliases/functions

--- zsh/init.d/aliases.sh
+++ zsh/init.d/aliases.sh
@@ -4,7 +4,6 @@ alias audio.analog='pacmd set-card-profile 0 output:analog-stereo+input:analog-s
 
 alias pm="sudo pacman"
 alias doc="docker-compose"
-alias k="kubectl"
 
 alias ls="ls -Fh --color=auto" # default ls options
 alias la="ls -la"
--- zsh/init.d/k8s.sh
+++ zsh/init.d/k8s.sh
@@ -1,19 +1,53 @@
 source <(kubectl completion zsh)
 
+alias k="kubectl"
+alias mk="minikube"
+alias k.gap="kubectl get pods --all-namespaces"
+alias k.wgap="watch kubectl get pods --all-namespaces"
+
+# usage: mk.start [args]
 function mk.start() {
-  minikube start --vm-driver=virtualbox --memory=4096
-  minikube ssh -- sudo ln -s /hosthome/$USER /home/$USER
+    minikube start --memory=4096 $*
+    minikube ssh -- sudo ln -s /hosthome/$USER /home/$USER
+}
+
+# usage: mk.start2 [args]
+function mk.start2() {
+    minikube start $*
+    minikube ssh -- sudo ln -s /hosthome/$USER /home/$USER
+}
+
+# usage: mk.load [filter]
+function mk.load() {
+    docker images |
+    awk '$1 ~ "^quay.io/weaveworks" && $2 == "latest" {print $1 ":" $2}' |
+    grep $1 |
+    while read img; do
+        echo $img
+        docker save $img |
+        (eval $(minikube docker-env) && docker load)
+    done
 }
 
 function mk.env() {
-  eval $(minikube docker-env)
+    eval $(minikube docker-env)
 }
 
-function mk.load() {
-  docker images |
-  awk '$1 ~ "^quay.io/weaveworks" && $2 == "latest" {print $1 ":" $2}' |
-  while read img; do
-    docker save $img |
-    (eval $(minikube docker-env) && docker load)
-  done
+function mk.envu() {
+    eval $(minikube docker-env -u)
+}
+
+function mk.reload() {
+    kubectl get pods --all-namespaces -o json |
+    jq -r --arg image "$1" '
+    .items[]|
+    select(
+        .spec.containers | any(.image == $image)
+        )|
+        .metadata|
+        "\(.namespace) \(.name)"
+        ' | while read namespace name; do
+        echo "Deleting $namespace/$name"
+        kubectl delete pod --namespace="$namespace" "$name"
+    done
 }
